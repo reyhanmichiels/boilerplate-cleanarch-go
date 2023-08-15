b11d864cdb2e90b22b6635e50284221346cf4265
reyhanmichiels <michiels.reyhan@gmail.com>
2023-08-15 23:54:24 +0700

    feat(bootstrap): call the function for bootstraping

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"boilerplate-cleanarch/bootstrap"
+	"fmt"
+	"os"
+)
+
 func main() {
+
+	if len(os.Args) == 1 {
+
+		fmt.Println("you must specified the project name")
+		return
+
+	}
+
+	projectName := os.Args[1]
+	err := bootstrap.Generate(projectName)
+	if err != nil {
+
+		fmt.Println(err.Error())
+
+	}
 
 }
